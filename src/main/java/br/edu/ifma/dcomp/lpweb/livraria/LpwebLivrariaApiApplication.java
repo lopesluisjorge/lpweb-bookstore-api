7e07fb68167537e1442d3f0333a1d9d4589a19ef
Jorge Luis L. Amorim <lopesluisjorge@hotmail.com>
2019-04-23 23:30:49 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifma/dcomp/lpweb/livraria/LpwebLivrariaApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifma.dcomp.lpweb.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpwebLivrariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpwebLivrariaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifma/dcomp/lpweb/livraria/LpwebLivrariaApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.ifma.dcomp.lpweb.livraria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LpwebLivrariaApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
